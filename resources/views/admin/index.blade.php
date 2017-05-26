d7cb5ebe3c1f3d506b55bb1201dcc8a35c91573f
palchik <andrey.palchik@itrexgroup.com>
2017-05-26 13:33:25 +0300

    fix https font

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
 
     <title>CV Generator</title>
     <!--Import Google Icon Font-->
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/css/materialize.min.css">
     <!--Let browser know website is optimized for mobile-->
